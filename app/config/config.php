ae7c2c09f47b9bdb972fa78d46d1f9c16ad42463
Devin <devdhera@gmail.com>
2019-09-06 20:11:44 +0530

    Added app configs

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost/tiny-php-mvc');
+    // Site Name
+    define('SITENAME', 'TiNY PHP MVC');
