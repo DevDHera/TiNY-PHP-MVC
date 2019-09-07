2297c89de2895727d2a91ecc59ea5fe610aff190
Devin <devdhera@gmail.com>
2019-09-07 12:02:28 +0530

    Added DB configs

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,11 @@
 <?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_PORT', '3308');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'tpmvc');
+
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
